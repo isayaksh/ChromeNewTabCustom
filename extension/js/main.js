4a258c2a54ccb91acf30cc25408b263e76ae6526
isayaksh <isayaksh@gmail.com>
2024-04-16 20:51:46 +0900

    <feat #3> 저장한 url, block에 그려 화면에 표현하기

--- extension/js/main.js
+++ extension/js/main.js
@@ -13,10 +13,8 @@ chrome.storage.local.get('urls', (data) => {
     const ulElement = document.getElementById('urls-list');
     
     urls.forEach(obj => {
-        const liElement = document.createElement('li');
-        const textNode = document.createTextNode(`${obj.date}: ${obj.url}`);
-        liElement.appendChild(textNode);
-        ulElement.appendChild(liElement);
+        addMemoToBoard(obj.date, obj.url);
+
     });
 
 });
@@ -27,3 +25,34 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     const urlsElement = document.getElementById('urls');
     urlsElement.textContent = urls.join(', '); // 예시: 배열을 문자열로 변환하여 출력
 });
+
+// memo를 만듭니다.
+function createMemo(title, body) {
+    const memo = document.createElement('div');
+    memo.classList.add('memo');
+
+    const header = document.createElement('div');
+    header.classList.add('header');
+    header.textContent = title;
+
+    const bodyDiv = document.createElement('div');
+    bodyDiv.classList.add('body');
+
+    const bodyLink = document.createElement('a');
+    bodyLink.href = body;
+    bodyLink.textContent = body;
+    bodyLink.target = '_blank'
+
+    bodyDiv.appendChild(bodyLink);
+    memo.appendChild(header);
+    memo.appendChild(bodyDiv);
+
+    return memo;
+}
+
+// board에 memo를 추가합니다.
+function addMemoToBoard(title, body) {
+    const board = document.getElementById('urls-list');
+    const memo = createMemo(title, body);
+    board.appendChild(memo);
+}
