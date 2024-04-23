d367a7d7f99355dd9c3fe6dea82dd8a273cbc2a2
isayaksh <isayaksh@gmail.com>
2024-04-23 19:23:49 +0900

    <feat #5> .dot-cancel 버튼을 클릭하여 .memo 엘리먼트 삭제

--- extension/js/main.js
+++ extension/js/main.js
@@ -9,12 +9,9 @@ function searchGoogle() {
 
 chrome.storage.local.get('urls', (data) => {
     const urls = data.urls;
-
-    const ulElement = document.getElementById('urls-list');
     
     urls.forEach(obj => {
         addMemoToBoard(obj.date, obj.url);
-
     });
 
 });
@@ -26,9 +23,36 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     urlsElement.textContent = urls.join(', '); // 예시: 배열을 문자열로 변환하여 출력
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const dotCancel = document.querySelectorAll('.dot-cancel');
+    dotCancel.forEach(function(cancelButton) {
+        cancelButton.addEventListener('click', function() {
+            const memo = cancelButton.closest('.memo'); // .memo 엘리먼트 찾기
+            memo.style.display = 'none'; // .memo 엘리먼트 숨기기
+
+            const date = memo.querySelector('.header').textContent;
+
+            console.log('select date : ' + date);
+
+            // 저장된 데이터를 가져와서 삭제할 데이터를 찾고 제거합니다.
+            chrome.storage.local.get('urls', (data) => {
+                const urls = data.urls;
+                // 삭제할 데이터를 찾아서 제거
+                const updatedUrls = urls.filter(obj => obj.date !== date);
+                // 수정된 데이터를 다시 저장
+                chrome.storage.local.set({ 'urls': updatedUrls }, () => {
+                    console.log('Data updated successfully.');
+                });
+            });
+
+        });
+    });
+});
+
 // memo를 만듭니다.
 function createMemo(title, body) {
     const memo = document.createElement('div');
+    memo.setAttribute('id', title);
     memo.classList.add('memo');
 
     const header = document.createElement('div');
